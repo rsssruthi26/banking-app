d9a745ba8673e82778bcb05681d8ac155dce49ee
Sai Satya Sruthi <saisatyasruthi@Sais-MacBook-Pro.local>
2024-05-20 17:15:59 -0400

    updated sysout

--- Banking-App/src/main/java/sruthi/com/bankingapp/BankingAppApplication.java
+++ Banking-App/src/main/java/sruthi/com/bankingapp/BankingAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Banking Application!");
 		SpringApplication.run(BankingAppApplication.class, args);
 	}
 
